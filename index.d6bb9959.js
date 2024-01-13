784a0bd5f9fdd0094c0854081df24782d61427cc
JuanMartinRivas <jmartinrivas14@gmail.com>
2024-01-13 11:35:27 -0300

    Updates

---
+++ index.d6bb9959.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".toggle-container__input"),e=document.querySelectorAll(".card__pricing-span");function n(){t.checked?(e[0].textContent="19.99",e[1].textContent="24.99",e[2].textContent="39.99"):(e[0].textContent="199.99",e[1].textContent="249.99",e[2].textContent="399.99")}t.addEventListener("click",n),window.addEventListener("load",n);
+//# sourceMappingURL=index.d6bb9959.js.map
